bcaa4dc3ce4db8bf371a21d5421913b87fb9521b
Emily Chew <emilychewsh@gmail.com>
2024-03-13 17:13:09 +1100

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App () {
+    const fetchAPI = "https://dog.ceo/api/breeds/image/random"
+
+    const [image, setImage] = useState(null)
+
+    useEffect(() => {
+        fetch(fetchAPI)
+            .then((resp) => resp.json())
+            .then((data) => (setImage(data.message)))
+
+            .catch(error => {
+                console.error("Error fetching dog image", error)
+            })
+    }, [])
+
+    if (!image) {
+        return <p>Loading...</p>
+    }
+
+
+    return (
+        <div>
+            <img src={image} alt="A Random Dog" />
+        </div>
+    )
+}
+
+export default App 
